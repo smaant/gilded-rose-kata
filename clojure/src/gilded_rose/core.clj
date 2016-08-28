972f278b49b62f0426f81c6e63098f731099cfcf
Anton Panferov <smaant@gmail.com>
2016-08-28 23:05:33 +0300

    add types

--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -3,6 +3,18 @@
 (def ^:const min-quality 0)
 (def ^:const max-quality 50)
 
+;; Have a polymorphism based on items names is not a very scalable solution.
+;; Better solution is to have types of items that behave in a similar way.
+;; Assuming we cannon change or extend item (by the kata definition),
+;; external mapping is the only solution for adding types. Not ideal
+;; one though, because requires updating this hash-map for each new name.
+(def ^:const items-types {"Conjured Belt"                             :conjured
+                          "Backstage passes to a TAFKAL80ETC concert" :ticket
+                          "Sulfuras, Hand Of Ragnaros"                :legendary
+                          "Elixir of the Mongoose"                    :normal
+                          "Aged Brie"                                 :improving
+                          "+5 Dexterity Vest"                         :normal})
+
 (defn- dec-min
   "Decrease given `val` by `dec-by` (or 1), but not lower than `min-val`"
   ([val min-val]        (dec-min val 1 min-val))
@@ -13,9 +25,9 @@
   [val max-val]
   (min (inc val) max-val))
 
-(defmulti update-item :name)
+(defmulti update-item (comp items-types :name))
 
-(defmethod update-item "+5 Dexterity Vest"
+(defmethod update-item :normal
   [item]
   (-> item
       (update :sell-in dec)
@@ -24,22 +36,13 @@
           (neg? sell-in) (update :quality dec-min min-quality)
           :always        (update :quality dec-min min-quality)))))
 
-(defmethod update-item "Elixir of the Mongoose"
-  [item]
-  (-> item
-      (update :sell-in dec)
-      (as-> {:keys [sell-in] :as item*}
-        (cond-> item*
-          (neg? sell-in) (update :quality dec-min min-quality)
-          :always        (update :quality dec-min min-quality)))))
-
-(defmethod update-item "Aged Brie"
+(defmethod update-item :improving
   [item]
   (-> item
       (update :sell-in dec)
       (update :quality inc-max max-quality)))
 
-(defmethod update-item "Backstage passes to a TAFKAL80ETC concert"
+(defmethod update-item :ticket
   [item]
   (-> item
       (update :sell-in dec)
@@ -50,11 +53,11 @@
           (< sell-in 5)  (update :quality inc-max max-quality)
           (neg? sell-in) (assoc :quality 0)))))
 
-(defmethod update-item "Sulfuras, Hand Of Ragnaros"
+(defmethod update-item :legendary
   [item]
   item)
 
-(defmethod update-item "Conjured Belt"
+(defmethod update-item :conjured
   [item]
   (-> item
       (update :sell-in dec)
